e915cac2b5ff4f5a993c93c8569bb6f89fdd5759
dmtweedy <damon.tweedy@yahoo.com>
2023-08-28 15:36:03 -0500

    configured loaders and workbox plugins

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,9 +3,6 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
-
 module.exports = () => {
   return {
     mode: 'development',
@@ -18,12 +15,43 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      // HTML file from a template
+      new HtmlWebpackPlugin({
+        template: './src/index.html',
+        chunks: ['main'],
+      }),
+
+      // Web App Manifest
+      new WebpackPwaManifest({
+        name: 'Just Another Text Editor',
+        short_name: 'J.A.T.E',
+        description: 'A simple text editor Progressive Web App',
+        background_color: '#ffffff',
+        theme_color: '#31a9e1',
+        icons: [
+          {
+            src: path.resolve('src/images/icon.png'),
+            sizes: [96, 128, 192, 256, 384, 512], // multiple sizes for different devices
+            destination: path.join('assets', 'icons'),
+          },
+        ],
+      }),
+
+      // Injects service worker
+      new InjectManifest({
+        swSrc: './src-sw.js', // Path to script
+        swDest: 'service-worker.js', // Output file name
+        exclude: [/\.map$/, /manifest\.json$/, /_redirects$/], // Files to exclude
+      }),
     ],
 
     module: {
       rules: [
-        
+        // Add CSS loaders
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader'],
+        },
       ],
     },
   };
